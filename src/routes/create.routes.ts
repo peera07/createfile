0fdd2c63c7e050655a765eef283d4cdec4e15b35
Peerapong Kraisrisuwan <p.kraisrisuwan@gmail.com>
2020-02-13 08:55:50 +0700

    change encode

--- src/routes/create.routes.ts
+++ src/routes/create.routes.ts
@@ -1,10 +1,13 @@
 import { Request, Response, Router, NextFunction } from "express";
 const fs = require('fs');
+const iconv  = require('iconv-lite');
 class fileRoutes {
   createFile() {
     return async (req: Request, res: Response, next: NextFunction) => {
       const { text, ext, path } = req.body;
-      await fs.writeFile(`${path}/${ext}`, `${text}`, {encoding: 'utf-8'});
+      //let body = iconv.decode(new Buffer(text), "TIS-620");
+      //console.log(body)
+      await fs.writeFile(`${path}/${ext}`, `${text}`, {encodeing: 'tis620'});
       return res.status(200).send({message: 'Success'})
     };
   }
